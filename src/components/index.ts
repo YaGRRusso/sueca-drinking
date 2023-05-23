4abd1aa5be9315a2f0d2f4deace19becee0d193a
YaGRRusso <yagrrusso@gmail.com>
2023-05-23 01:09:42 -0300

    frist commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,2 @@
 export { default as ErrorBoundary } from './ErrorBoundary'
 export { default as Head } from './Head'
-
-export { default as Input } from './Input'
-export type { InputProps } from './Input'
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,15 @@ import { HeaderLayout } from '@/layouts'
 import '@/styles/globals.css'
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppProvider>
-        <HeaderLayout>
-          <Head title="Next App" />
-          <Component {...pageProps} />
-        </HeaderLayout>
-      </AppProvider>
-    </QueryClientProvider>
+    <AppProvider>
+      <HeaderLayout>
+        <Head title="Next App" />
+        <Component {...pageProps} />
+      </HeaderLayout>
+    </AppProvider>
   )
 }
 
