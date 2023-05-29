e79eee4414b4f7ea8d27b769d24afe8ceed50715
YaGRRusso <yagrrusso@gmail.com>
2023-05-29 01:23:25 -0300

    swipe func

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import clsx from 'clsx'
 import { GetStaticProps, NextPage } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { useCallback, useState } from 'react'
+import { TouchEvent, useCallback, useState } from 'react'
 
 const HomePage: NextPage = ({}) => {
   const { t: tCommon } = useTranslation('common')
@@ -13,9 +13,13 @@ const HomePage: NextPage = ({}) => {
   const { deck, getLastDeckCard, resetDeck } = useDeckContext()
   const [card, setCard] = useState<CardProps>()
   const [flipped, setFlipped] = useState(false)
+  const [swipeStart, setSwipeStart] = useState<number>()
+  const [swipeEnd, setSwipeEnd] = useState<number>()
+  const [swipeDirection, setSwipeDirection] = useState<'left' | 'right'>()
 
   const handleGetLastCard = useCallback(() => {
     setCard(getLastDeckCard())
+    setFlipped(false)
   }, [getLastDeckCard])
 
   const handleResetDeck = useCallback(() => {
@@ -23,20 +27,74 @@ const HomePage: NextPage = ({}) => {
     setCard(undefined)
   }, [resetDeck])
 
+  const handleGetSwipeDirection = useCallback(
+    (current: number) => {
+      if (swipeStart) {
+        swipeStart > current
+          ? setSwipeDirection('left')
+          : setSwipeDirection('right')
+      }
+    },
+    [swipeStart]
+  )
+
+  // const handleMouseStart = useCallback((ev: MouseEvent<HTMLButtonElement>) => {
+  //   setSwipeStart(ev.clientX)
+  // }, [])
+
+  // const handleMouseMove = useCallback(
+  //   (ev: MouseEvent<HTMLButtonElement>) => {
+  //     handleGetSwipeDirection(ev.clientX)
+  //     setSwipeEnd(ev.clientX)
+  //   },
+  //   [handleGetSwipeDirection]
+  // )
+
+  const handleTouchStart = useCallback((ev: TouchEvent<HTMLButtonElement>) => {
+    const touch = ev.touches[0]
+    setSwipeStart(touch.clientX)
+  }, [])
+
+  const handleTouchMove = useCallback(
+    (ev: TouchEvent<HTMLButtonElement>) => {
+      const touch = ev.touches[0]
+      handleGetSwipeDirection(touch.clientX)
+      setSwipeEnd(touch.clientX)
+    },
+    [handleGetSwipeDirection]
+  )
+
+  const handleSwipe = useCallback(() => {
+    if (swipeStart && swipeEnd) {
+      handleGetLastCard()
+      setSwipeDirection(undefined)
+      setSwipeStart(undefined)
+      setSwipeEnd(undefined)
+    }
+  }, [handleGetLastCard, swipeEnd, swipeStart])
+
   return (
-    <div className="container-center container flex flex-col gap-4 font-xyz">
+    <div className="container-center container flex flex-col gap-4 font-amatic">
       <span
         className={clsx(
           'flex items-center justify-center gap-2 text-xl',
           deck.length === 0 && 'text-red-500'
         )}
+        onClick={() => console.log(deck)}
       >
         {tCommon('cards', { count: deck.length })}
       </span>
       <Flip
+        className={clsx(
+          'flex aspect-card w-full max-w-sm items-center justify-center',
+          swipeDirection === 'left' && '-translate-x-5 -rotate-1',
+          swipeDirection === 'right' && 'translate-x-5 rotate-1'
+        )}
         isFlipped={flipped}
         onClick={() => setFlipped(!flipped)}
-        className="flex aspect-card w-full max-w-sm items-center justify-center"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleSwipe}
       >
         <Card.Front value={card?.value} suit={card?.suit} />
         <Card.Back rule={tRules(card?.value.toString() || '')} />
